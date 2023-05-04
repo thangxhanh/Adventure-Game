69b77eacbb30a27349e404efb7ca697384eade57
thangxhanh <lumanhthangvblcvn@gmail.com>
2023-05-04 13:39:55 +0700

    add code

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     private AudioSource finishSource;
+    private bool levelCompleted = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +17,15 @@ public class Finish : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "player") {
+        if (collision.gameObject.name == "Player" && !levelCompleted) {
             finishSource.Play();
-            CompleteLevel();
+            levelCompleted = true;
+            Invoke("CompleteLevel", 2f);
         }
     }
 
     private void CompleteLevel()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
